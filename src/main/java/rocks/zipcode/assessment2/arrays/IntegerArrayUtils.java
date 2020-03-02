a029f3cc553e5edd66e8f6e83d1ac12a8dc56a1f
chipfody <60615532+chipfody@users.noreply.github.com>
2020-03-02 09:56:16 -0500

    commit2

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -29,7 +29,8 @@ public class IntegerArrayUtils {
      * @return `integerArray` with `valueToBeInserted` at index number `indexToInsertAt`
      */
     public static Integer[] replace(Integer[] integerArray, int indexToInsertAt, Integer valueToBeInserted) {
-        return null;
+        integerArray[indexToInsertAt] = valueToBeInserted;
+        return integerArray;
     }
 
     /**
@@ -38,7 +39,7 @@ public class IntegerArrayUtils {
      * @return element located at `indexToFetch`
      */
     public static Integer get(Integer[] integerArray, Integer indexToFetch) {
-        return null;
+        return integerArray[indexToFetch];
     }
 
     /**
@@ -46,7 +47,13 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1 and odd-values decremented by 1
      */
     public static Integer[] incrementEvenDecrementOdd(Integer[] integerArray) {
-        return null;
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                integerArray[i] = (integerArray[i] + 1);
+            }else{ integerArray[i] = integerArray[i] - 1;
+                    }
+                }
+        return integerArray;
     }
 
     /**
@@ -54,7 +61,12 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                integerArray[i] = (integerArray[i] + 1);
+            }
+        }
+            return integerArray;
     }
 
     /**
@@ -62,6 +74,11 @@ public class IntegerArrayUtils {
      * @return identical array with odd-values decremented by 1
      */
     public static Integer[] decrementOdd(Integer[] input) {
-        return null;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 == 1) {
+                input[i] = (input[i] - 1);
+            }
+        }
+        return input;
     }
 }
