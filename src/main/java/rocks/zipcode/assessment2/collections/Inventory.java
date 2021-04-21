2db260a7315fe1d936bd028048beaaa93e397e22
chipfody <60615532+chipfody@users.noreply.github.com>
2021-04-21 11:28:46 -0400

    revised inventory class

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,58 +1,56 @@
 package rocks.zipcode.assessment2.collections;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
-    private List <String> items;
-    private Map <String, Integer> inventory;
 
+    private List<String> items;
+    private Map<String, Integer> inventory;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
+
         this.items = strings;
         inventory = new HashMap<>();
-        for (String item : items)
-            if (inventory.containsKey(item)) {
+        for(String item : strings) {
+            if(!inventory.containsKey(item)){
+                inventory.put(item,1);
+            } else{
                 inventory.put(item, inventory.get(item) + 1);
-            } else {
-                inventory.put(item, 1);
             }
-
+        }
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-        Map <String, Integer> inventory = new HashMap<>();
-
+        this.items = new ArrayList<>();
+        this.inventory = new HashMap<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
+
         if (inventory.containsKey(item)) {
-            inventory.put(item, inventory.get(item) + 1);
+            inventory.put(item, (inventory.get(item) + 1));
         } else {
             inventory.put(item, 1);
         }
-       }
 
+    }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-
-        inventory.remove(item);
+        inventory.put(item, (inventory.get(item) - 1));
     }
 
     /**
@@ -60,6 +58,12 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+
+        if(inventory.containsKey(item)){
+            return inventory.get(item);
+        } else {
+            return 0;
+        }
     }
+
 }
